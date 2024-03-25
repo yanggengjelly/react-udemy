79a8c92170acc77c6dcbe318219eb48d6fd43369
YANGJUNHO <yangwork96@gmail.com>
2024-03-26 00:52:17 +0900

    add

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,9 +4,12 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(!isEditing);
+    //setIsEditing(!isEditing);
     //setIsEditing(!isEditing ? false : true);
-    // (!isEditing) = isEditing가 true면 false를 반환하고 false면 true로 반환
+    //(!isEditing) = isEditing가 true면 false를 반환하고 false면 true로 반환
+    setIsEditing((editing) => !editing);
+    // 이렇게 함수를 쓰면 상태 가 가장 최신 버전으로 유지되면서
+    // 상태 변경 값 이전의 상태값을 기반으로 변경해야됨
   }
 
   let playerName = <span className="player-name">{name}</span>;
